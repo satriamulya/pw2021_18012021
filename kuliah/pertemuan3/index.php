eb00770345eea1367f265b6947e5661502d61925
Satria Mulya <satriamulya.9@gmail.com>
2021-01-23 20:32:16 +0700

    serching data

--- kuliah/pertemuan3/index.php
+++ kuliah/pertemuan3/index.php
@@ -4,6 +4,12 @@ require 'functions.php';
 
 // tampung ke variable mahasiswa  
 $mahasiswa =  query("SELECT * FROM mahasiswa");
+
+// ketika tombol cari diklik
+if (isset($_POST['cari'])) {
+  $mahasiswa = cari($_POST['keyword']);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +25,15 @@ $mahasiswa =  query("SELECT * FROM mahasiswa");
   <h3>Daftar Mahasiswa</h3>
   <a href="tambah.php">Tambah Data Mahasiswa</a>
   <br><br>
+
+
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="30" placeholder="masukkan keyword pencarian.." autocomplete="off" autofocus>
+    <button type="submit" name="cari">Cari!</button>
+  </form>
+  <br>
+
+
   <table border="1" cellpadding="10" cellspacing="0">
     <tr>
       <th>#</th>
@@ -26,6 +41,14 @@ $mahasiswa =  query("SELECT * FROM mahasiswa");
       <th>Nama</th>
       <th>Aksi</th>
     </tr>
+
+    <?php if (empty($mahasiswa)) : ?>
+      <tr>
+        <td colspan="4">
+          <p style="color: red; font-style: italic;">data mahasiswa tidak ditemukan!</p>
+        </td>
+      </tr>
+    <?php endif; ?>
 
     <?php $i = 1;
     foreach ($mahasiswa as $m) : ?>
